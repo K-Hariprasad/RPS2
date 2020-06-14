9f4cc47ed293ab7b9bfecbb608f7681da322538b
Hari <hariprasadk125@gmail.com>
2020-06-14 13:29:49 +0530

    modified result page and play again button added

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 class Result extends React.Component{
+    refresh=()=>{
+        window.location.reload()
+    }
     render(){
         return(
             <React.Fragment>
                 
                 <div id="result">
-                    <p className="text-center">{this.props.greetings} {this.props.username}</p><br/>
-                    <p className="text-center">{this.props.msg}</p>
+                    <div className="result">
+                    <p className="text-center result-greet">{this.props.greetings} {this.props.username}</p><br/>
+                    <p className="text-center result-msg display-4">{this.props.msg}</p><br/>
+                    <button className="btn btn-primary" onClick={this.refresh}>Play again</button>
+                    </div>
                 </div>
             </React.Fragment>
         )
