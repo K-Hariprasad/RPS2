a437404bc5bf0a15027ca8f0240ad85fe06dcbc3
Hari <hariprasadk125@gmail.com>
2020-06-14 12:47:21 +0530

    Updates

---
+++ precache-manifest.28cb04ec811c29210bc50fadab6a105f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef777c35e50a7e2b49b921a6801fda66",
+    "url": "/RPS2/index.html"
+  },
+  {
+    "revision": "8ab30ca016e83a9099f5",
+    "url": "/RPS2/static/css/main.7602f5c0.chunk.css"
+  },
+  {
+    "revision": "9eb6c81b4a937baac87a",
+    "url": "/RPS2/static/js/2.25120512.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RPS2/static/js/2.25120512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab30ca016e83a9099f5",
+    "url": "/RPS2/static/js/main.4f0ef0d9.chunk.js"
+  },
+  {
+    "revision": "2a18c45245e3e97ab9bb",
+    "url": "/RPS2/static/js/runtime-main.a7e7f2ee.js"
+  },
+  {
+    "revision": "a84f58becf0e1bd9e402871b31521830",
+    "url": "/RPS2/static/media/2766672.a84f58be.jpg"
+  }
+]);
